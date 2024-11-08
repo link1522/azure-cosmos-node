2e1767ad078938457ffc27855d8825587af92ad2
Link1515 <h26281660@gmail.com>
2024-11-08 14:10:36 +0800

    add find method

--- helpers/DBHelper.ts
+++ helpers/DBHelper.ts
@@ -1,6 +1,18 @@
+import { ItemDefinition } from 'npm:@azure/cosmos';
 import { database } from '../dbConn.ts';
 
 export async function upsert<T>(containerName: string, item: T) {
   const container = database.container(containerName);
   await container.items.upsert(item);
+}
+
+export async function find<T extends ItemDefinition>(
+  containerName: string,
+  id: string,
+  partitionKey: string
+) {
+  const container = database.container(containerName);
+  const response = await container.item(id, partitionKey).read<T>();
+  const item = response.resource;
+  return item;
 }
--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
 import 'jsr:@std/dotenv/load';
-import { upsert } from './helpers/DBHelper.ts';
+import type { Product } from './interface/Product.ts';
+import { upsert, find } from './helpers/DBHelper.ts';
 
-const item = {
+const item: Product = {
   id: '123456',
   categoryId: 'fruit',
-  name: 'apple'
+  name: 'apple',
+  price: 200
 };
-upsert('Test', item);
+
+// upsert('Test', item);
+
+const result = await find<Product>('Test', '123456', 'fruit');
+console.log(result);
 
 console.log('Done!');
